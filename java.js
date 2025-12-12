6ef789ba84514c14776467f593423217d2abd2cd
flutsie <129947164+flutsie@users.noreply.github.com>
2025-12-12 13:25:20 +0100

    Update tracking number for redirection

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 document.getElementById("trackBtn").addEventListener("click", function () {
   const input = document.getElementById("trackingInput").value.trim().toLowerCase();
 
-  if (input === "98775432222211100") {
+  if (input === "9877543322222111000") {
     window.location.href = "001.html";
   } else {
     alert("Oei… dat trackingnummer bestaat niet");
